a73403b1d95d788e78428aa38d889e532f948915
Justin Lovell <justin@justjuzzy.com>
2012-10-31 16:46:23 +0200

    Initial commit

---
+++ ExceptionsFromAwaitableCode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsFromAwaitableCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoAsyncOperationAndWait().Wait();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Main() method:");
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static async Task DoAsyncOperationAndWait()
+        {
+            try
+            {
+                await DoAsyncOperationAsTask();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DoAsyncOperationAndWait() method:");
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static Task DoAsyncOperationAsTask()
+        {
+            return Task.Run(() =>
+                                {
+                                    throw new Exception("Hello there!");
+                                });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+About
+=====
+Ever wondered how an `Exception` stack trace would look like on `awaitable` code?
+
+This small example will demonstrate how the exception is caught and the stack trace would be recorded.
+
+Blog Post
+---------
+This example was generated to assist in the discussion of this topic. You may read more information:
+
+
